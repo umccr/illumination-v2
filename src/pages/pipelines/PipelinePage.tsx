505db657d1d61c61889ecd6ad0688fb59c64ec8f
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-04-28 14:42:38 +1000

    Add pipeline page (#9)

---
+++ src/pages/pipelines/PipelinePage.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+
+// React-Router-Dom components
+import { useParams } from "react-router-dom";
+
+// MUI Component
+import { CircularProgress, Grid, Typography } from "@mui/material";
+
+// icats Component
+import { Pipeline, PipelineApiAxiosParamCreator, RunAxios } from "icats";
+
+// Custom components
+import { useDialogContext } from "../../container/app/DialogContext";
+import JSONContainer from "../../components/JSONContainer/JSONContainer";
+import ChipArray, { IButtonProps } from "../../components/chipArray/ChipArray";
+
+const buttonProps: IButtonProps[] = [
+  { name: "Input Parameters", route: "inputParameters" },
+  { name: "Reference Sets", route: "referenceSets" },
+];
+
+async function getPipelineData(pipelineId: string): Promise<Pipeline> {
+  // Generate axios parameter
+  const PipelineParamCreator = PipelineApiAxiosParamCreator();
+  const getPipelinesParam = await PipelineParamCreator.getPipeline(pipelineId);
+
+  // Calling axios
+  const axiosData = await RunAxios(getPipelinesParam);
+  return axiosData.data;
+}
+
+function PipelinePage() {
+  const { pipelineId } = useParams();
+
+  const { setDialogInfo } = useDialogContext();
+  const [pipelineResponse, setPipelineResponse] = useState<Pipeline | null>();
+
+  useEffect(() => {
+    let cancel = false;
+    async function fetchData() {
+      try {
+        if (pipelineId) {
+          const data = await getPipelineData(pipelineId);
+          if (cancel) return;
+          setPipelineResponse(data);
+        }
+      } catch (err) {
+        setDialogInfo({
+          isOpen: true,
+          dialogTitle: "Error",
+          dialogContent: `Sorry, An error has occured while fetching the API (${err}). Please try again!`,
+        });
+      }
+    }
+    fetchData();
+    return () => {
+      cancel = true;
+    };
+  }, [pipelineId, setDialogInfo]);
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      spacing={3}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h4">Pipeline</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="subtitle1">Pipeline Id: {pipelineId}</Typography>
+      </Grid>
+      <ChipArray data={buttonProps} />
+      {!pipelineResponse ? (
+        <CircularProgress sx={{ marginTop: "50px" }} />
+      ) : (
+        <Grid item container spacing={3}>
+          <Grid item xs={12}>
+            <JSONContainer data={pipelineResponse} />
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
+  );
+}
+
+export default PipelinePage;
---
+++ src/pages/pipelines/PipelinesPage.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+
+// MUI components
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import CircularProgress from "@mui/material/CircularProgress";
+
+// icats component
+import { PipelineList, PipelineApiAxiosParamCreator, RunAxios } from "icats";
+
+// Custom components
+import JSONContainer from "../../components/JSONContainer/JSONContainer";
+import { useDialogContext } from "../../container/app/DialogContext";
+import CustomTable, { IColumnMapping } from "../../container/table/Table";
+
+const COLUMN_MAPPPING: IColumnMapping[] = [
+  {
+    displayName: "ID",
+    jsonKeys: ["id"],
+    linkTo: { formatString: "{0}", formatValue: [["id"]] },
+  },
+  { displayName: "Owner Id", jsonKeys: ["ownerId"] },
+  { displayName: "Code", jsonKeys: ["code"] },
+  { displayName: "Description", jsonKeys: ["description"] },
+  { displayName: "Language", jsonKeys: ["language"] },
+  { displayName: "Time Created", jsonKeys: ["timeCreated"] },
+  { displayName: "Time Modified", jsonKeys: ["timeModified"] },
+];
+
+async function getPipelinesData(): Promise<PipelineList> {
+  // Generate axios parameter
+  const PipelinesParamCreator = PipelineApiAxiosParamCreator();
+  const getPipelinesParam = await PipelinesParamCreator.getPipelines();
+
+  // Calling axios
+  const axiosData = await RunAxios(getPipelinesParam);
+  return axiosData.data;
+}
+
+function PipelinesPage() {
+  const [projectListResponse, setPipelinesListResponse] =
+    useState<PipelineList | null>();
+
+  const { setDialogInfo } = useDialogContext();
+
+  useEffect(() => {
+    let cancel = false;
+
+    async function fetchData() {
+      try {
+        const data = await getPipelinesData();
+        if (cancel) return;
+
+        setPipelinesListResponse(data);
+      } catch (err) {
+        setDialogInfo({
+          isOpen: true,
+          dialogTitle: "Error",
+          dialogContent: `Sorry, An error has occured while fetching the API (${err}). Please try again!`,
+        });
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      cancel = true;
+    };
+  }, [ setDialogInfo]);
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      spacing={3}
+    >
+      <Grid item xs={12}>
+        <Typography variant="h4">Available Pipelines</Typography>
+      </Grid>
+
+      {!projectListResponse ? (
+        <CircularProgress sx={{ marginTop: "50px" }} />
+      ) : (
+        <Grid item container spacing={3}>
+          <Grid item xs={12}>
+            <CustomTable
+              items={projectListResponse.items}
+              columnMapping={COLUMN_MAPPPING}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <JSONContainer data={projectListResponse} />
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
+  );
+}
+
+export default PipelinesPage;
