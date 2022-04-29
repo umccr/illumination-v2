f7e92d8c2aae36fb93dc00ffa1cee3f3c70a2faa
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-04-29 12:00:52 +1000

    Prevent re-login when refresh

--- src/container/app/UserContext.tsx
+++ src/container/app/UserContext.tsx
@@ -1,4 +1,8 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
 
 // Interface
 interface iProviderprops {
@@ -11,12 +15,49 @@ const userContext = {
   setUser: (user: any) => {},
 };
 
+function getUser() {
+  return Auth.currentAuthenticatedUser()
+    .then((userData) => userData)
+    .catch(() => console.log("Not signed in"));
+}
+
 function UserContextProvider(props: iProviderprops) {
   const [user, setUser] = useState(userContext.user);
+  const [isAuthenticating, setIsAuthenticating] = useState<Boolean>(true);
 
+  useEffect(() => {
+    async function onLoad() {
+      try {
+        await Auth.currentSession();
+        getUser().then((userData) => setUser(userData));
+      } catch (e) {
+        if (e !== "No current user") {
+        }
+      }
+
+      setIsAuthenticating(false);
+    }
+    onLoad();
+  }, []);
+  
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      {props.children}
+      {isAuthenticating ? (
+        <Container
+          maxWidth="lg"
+          sx={{
+            display: "flex",
+            paddingTop: "25rem",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <CircularProgress sx={{ margin: "1rem" }} />
+          <Typography>Authenticating ...</Typography>
+        </Container>
+      ) : (
+        props.children
+      )}
     </UserContext.Provider>
   );
 }
